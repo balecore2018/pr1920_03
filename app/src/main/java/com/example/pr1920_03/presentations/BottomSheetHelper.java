e5d75c3cb8ab05888d455e181d358552bff19921
balecore <balecored@gmail.com>
2026-05-19 17:35:44 +0500

    меин активити Shapovalov

--- app/src/main/java/com/example/pr1920_03/presentations/BottomSheetHelper.java
+++ app/src/main/java/com/example/pr1920_03/presentations/BottomSheetHelper.java
@@ -7,49 +7,43 @@ import android.view.View;
 import com.example.pr1920_03.R;
 import com.example.uicomponents.BthBig;
 import com.example.uicomponents.BthCustom;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
-public class BottomSheetHelper {
+public class BottomSheetHelper extends BottomSheetDialog {
 
-    public BottomSheetHelper dialog;
+    public BottomSheetHelper(Context context) {
+        this(context, null, null);
+    }
 
+    public BottomSheetHelper(
+            Context context,
+            View.OnClickListener galleryClickListener,
+            View.OnClickListener cameraClickListener
+    ) {
+        super(context);
 
-    public BottomSheetHelper(Context context){
-
-        dialog = new BottomSheetHelper(context);
-
-        View view = LayoutInflater.from(context).inflate(R.layout.bs_select_image, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.bs_select_image, null, false);
 
         BthBig bthGallery = view.findViewById(R.id.bthGallery);
         BthBig bthCamera = view.findViewById(R.id.bthCamera);
 
-        bthGallery.init(
+        bthGallery.init("Выбрать из галереи", BthCustom.TypeButton.SECONDARY);
+        bthCamera.init("Сфотографировать", BthCustom.TypeButton.SECONDARY);
 
-                "\uD83D\uDDBC\uFE0F Выбрать из галереи",
-                BthCustom.TypeButton.SECONDARY
+        if (galleryClickListener != null) {
+            bthGallery.Bth.setOnClickListener(v -> {
+                galleryClickListener.onClick(v);
+                dismiss();
+            });
+        }
 
-        );
+        if (cameraClickListener != null) {
+            bthCamera.Bth.setOnClickListener(v -> {
+                cameraClickListener.onClick(v);
+                dismiss();
+            });
+        }
 
-        bthCamera.init(
-
-                "\uD83D\uDCF8 Сфотографировать",
-                BthCustom.TypeButton.SECONDARY
-
-        );
-
-        bthGallery.Bth.setOnClickListener(v -> {
-
-            ProductActivity.init.OpenGallery();
-
-        });
-
-        bthCamera.Bth.setOnClickListener(v -> {
-
-            ProductActivity.init.OpenCamera();
-
-        });
-
-        dialog.setContentView(view);
-
+        setContentView(view);
     }
-
 }
--- app/src/main/java/com/example/pr1920_03/presentations/MainActivity.java
+++ app/src/main/java/com/example/pr1920_03/presentations/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.pr1920_03.presentations;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +14,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.network.datas.products.ProductGet;
+import com.example.network.domains.callbacks.MyResponseCallback;
+import com.example.network.domains.models.Product;
 import com.example.pr1920_03.R;
+import com.example.pr1920_03.domains.PermissionManager;
+import com.example.uicomponents.BthBig;
+import com.example.uicomponents.BthCustom;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "PRODUCT GET";
+
+    private View bthOpenAddProduct;
+    private LinearLayout llContent;
+    private List<Product> products = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +46,55 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        findViewById(R.id.bthOpenAddProduct).setOnClickListener(v ->
+        PermissionManager.GetPermission(this, this);
+
+        bthOpenAddProduct = findViewById(R.id.bthOpenAddProduct);
+        llContent = findViewById(R.id.llContent);
+
+        bthOpenAddProduct.setOnClickListener(v ->
                 startActivity(new Intent(MainActivity.this, ProductActivity.class))
         );
+
+        productGet();
+    }
+
+    public void productGet() {
+        ProductGet requestProductGet = new ProductGet(new MyResponseCallback() {
+            @Override
+            public void onCompile(String result) {
+                Log.d(TAG, result);
+                products = new GsonBuilder().create().fromJson(
+                        result,
+                        new TypeToken<ArrayList<Product>>() {
+                        }.getType()
+                );
+                createElement();
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.e(TAG, error);
+            }
+        });
+
+        requestProductGet.execute();
+    }
+
+    public void createElement() {
+        llContent.removeAllViews();
+
+        for (Product product : products) {
+            View itemProduct = LayoutInflater.from(this).inflate(R.layout.tovar, llContent, false);
+
+            BthBig bthBig = itemProduct.findViewById(R.id.bthOpenAddProduct);
+            TextView tvName = itemProduct.findViewById(R.id.tvName);
+            TextView tvPrice = itemProduct.findViewById(R.id.tvPrice);
+
+            bthBig.init("Открыть", BthCustom.TypeButton.PRIMARY);
+            tvName.setText(product.name);
+            tvPrice.setText(product.price + " ₽");
+
+            llContent.addView(itemProduct);
+        }
     }
 }
