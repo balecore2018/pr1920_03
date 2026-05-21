7e4ea39e562e06c7a455e463cde42f66f680a324
balecore <balecored@gmail.com>
2026-05-22 03:55:52 +0500

    навбар активность мистер шаповалов

--- app/src/main/java/com/example/pr1920_03/presentations/MainActivity.java
+++ app/src/main/java/com/example/pr1920_03/presentations/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pr1920_03.presentations;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,12 +16,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.network.datas.products.ProductGetUser;
 import com.example.network.domains.callbacks.MyResponseCallback;
 import com.example.network.domains.common.Settings;
 import com.example.network.domains.models.Product;
 import com.example.pr1920_03.R;
+import com.example.pr1920_03.domains.callbacks.OnTabClickListner;
 import com.example.pr1920_03.domains.managers.PermissionManager;
 import com.example.uicomponents.BthBig;
 import com.example.uicomponents.BthCustom;
@@ -35,82 +39,53 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "PRODUCT GET USER";
     public static final String TOKEN = Settings.DEMO_TOKEN;
 
-    private View bthOpenAddProduct;
-    private LinearLayout llContent;
-    private List<Product> products = new ArrayList<>();
-
-    private final ActivityResultLauncher<Intent> productActivityLauncher =
-            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-                if (result.getResultCode() == RESULT_OK) {
-                    productGetUser();
-                }
-            });
+    public static MainActivity init;
+    public Fragment openFragment;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        init = this;
+        context = this;
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        NavigationMenu menu = new NavigationMenu(this, MenuItemSelect);
+        ft.add(R.id.menu_navigation, menu);
+        ft.commit();
 
-        PermissionManager.GetPermission(this, this);
-
-        bthOpenAddProduct = findViewById(R.id.bthOpenAddProduct);
-        llContent = findViewById(R.id.llContent);
-
-        bthOpenAddProduct.setOnClickListener(v ->
-                productActivityLauncher.launch(new Intent(MainActivity.this, ProductActivity.class))
-        );
-
-        productGetUser();
     }
 
-    public void productGetUser() {
-        ProductGetUser requestProductGetUser = new ProductGetUser(TOKEN, new MyResponseCallback() {
-            @Override
-            public void onCompile(String result) {
-                Log.d(TAG, result);
-                products = new GsonBuilder().create().fromJson(
-                        result,
-                        new TypeToken<ArrayList<Product>>() {
-                        }.getType()
-                );
+    OnTabClickListner MenuItemSelect = new OnTabClickListner() {
+        @Override
+        public void onTabClick(Integer position) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            if (openFragment != null)
+                ft.remove(openFragment);
 
-                if (products == null) {
-                    products = new ArrayList<>();
-                }
+            if (position == -1) {
 
-                createElement();
+                openFragment = new ProductFragment(context);
+                ft.add(R.id.content, openFragment);
+
+            } else if (position == 2) {
+
+                openFragment = new ProductsFragment(context, MenuItemSelect);
+                ft.add(R.id.content, openFragment);
+
             }
 
-            @Override
-            public void onError(String error) {
-                Log.e(TAG, error);
-            }
-        });
-
-        requestProductGetUser.execute();
-    }
-
-    public void createElement() {
-        llContent.removeAllViews();
-
-        for (Product product : products) {
-            View itemProduct = LayoutInflater.from(this).inflate(R.layout.tovar, llContent, false);
-
-            BthBig bthBig = itemProduct.findViewById(R.id.bthOpenAddProduct);
-            TextView tvName = itemProduct.findViewById(R.id.tvName);
-            TextView tvPrice = itemProduct.findViewById(R.id.tvPrice);
-
-            bthBig.init("Открыть", BthCustom.TypeButton.PRIMARY);
-            tvName.setText(product.name);
-            tvPrice.setText(product.price + " ₽");
-
-            llContent.addView(itemProduct);
+            ft.commit();
         }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        ProductFragment productFragment = (ProductFragment) openFragment;
+        productFragment.onActivityResult(requestCode, resultCode, data);
+
     }
 }
